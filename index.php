a257574b9317ddab134ef6dbe237adb88586d31d
Shashank Mistry <shashankmistry30@gmail.com>
2023-02-15 16:39:48 -0500

    Delete & Update with Postman file

--- index.php
+++ index.php
@@ -34,6 +34,30 @@ if (strpos($endpoint, '/products') !== false && $_SERVER['REQUEST_METHOD'] == 'G
 
     $stmt->execute();
     echo json_encode("New record created successfully");
+} elseif (strpos($endpoint, '/delete/products') !== false && $_SERVER['REQUEST_METHOD'] == 'DELETE') {
+    // Fetch data from the endpoint
+    $JSON = file_get_contents('php://input');
+    $data = json_decode($JSON, true);
+    $_POST = $data;
+    $stmt = $conn->prepare("DELETE FROM product WHERE id = :id");
+    $stmt->bindParam(':id', $_POST['id']);
+    $stmt->execute();
+    echo json_encode("Record deleted successfully with ID: " . $_POST['id'] );
+    //update
+} elseif (strpos($endpoint, '/update/products') !== false && $_SERVER['REQUEST_METHOD'] == 'PUT') {
+    // Fetch data from the endpoint
+    $JSON = file_get_contents('php://input');
+    $data = json_decode($JSON, true);
+    $_POST = $data;
+    $stmt = $conn->prepare("UPDATE product SET name = :name, description = :description, details = :details, image = :image, price = :price WHERE id = :id");
+    $stmt->bindParam(':id', $_POST['id']);
+    $stmt->bindParam(':name', $_POST['name']);
+    $stmt->bindParam(':description', $_POST['description']);
+    $stmt->bindParam(':details', $_POST['details']);
+    $stmt->bindParam(':image', $_POST['image']);
+    $stmt->bindParam(':price', $_POST['price']);
+    $stmt->execute();
+    echo json_encode("Record updated successfully");
 } else {
     // Return an error message
     echo json_encode("Error: Endpoint not found");
